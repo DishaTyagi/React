69a149ea63d89e0735a21de694772cac7724a189
DishaTyagi <omgdisha@gmail.com>
2020-06-01 03:07:28 +0530

    events, attributes and data binding

--- indecisionApp/public/scripts/app.js
+++ indecisionApp/public/scripts/app.js
@@ -6,7 +6,8 @@ console.log("app.js is running!");
 
 var object = {
     title: 'Dish',
-    subtitle: 'dish is a good girl'
+    // subtitle: 'dish is a good girl',
+    options: ['one', 'two']
 
     //JSX
 };var template = //this paranthesis is not actually required. just for clarity purposes.
@@ -18,43 +19,103 @@ React.createElement(
         null,
         object.title
     ),
-    React.createElement(
+    object.subtitle && React.createElement(
         'p',
         null,
         object.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        object.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'List item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'list item 2'
+        )
     )
 );
 
-var user = {
-    name: 'Disha',
-    age: 21,
-    location: 'vaishali'
+// const user = {
+//     name: 'Disha',
+//     age: 21,
+//     // location: 'vaishali'
+// }
+
+// function getLocation(location){
+//     if(location){
+//         return <p>Location: {location}</p>
+//     }
+//     //if location does not exist, then don't print anthing regarding location.
+// }
+
+// const templateTwo = (
+
+//     // jsx expressions must have a parent element. if we dont add this <div> as a parent element, there'll be an err
+//     <div>           
+//         <h1>{user.name}</h1>
+//         <p>Age: {user.age}</p>
+//         {getLocation(user.location)}
+
+//         {/*<h1>this is my h1 inside a pair of {`{}`} in jsx expression</h1>*/}
+//     </div>
+// )
+
+
+//to understand events and attributes
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    reRender();
+};
+var deleteOne = function deleteOne() {
+    count--;
+    reRender();
+};
+var reset = function reset() {
+    count = 0;
+    reRender();
 };
 
-var templateTwo =
-
-// jsx expressions must have a parent element. if we dont add this <div> as a parent element, there'll be an err
-React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
-);
-
 var appRoute = document.getElementById('appDiv');
-ReactDOM.render(template, appRoute); //1st -> what to render, 2nd param-> where to render
+
+//after changing the count val, updated count is not rendered. So, we have to render templateTwo initially and after updating the count.
+var reRender = function reRender() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            'Increment'
+        ),
+        React.createElement(
+            'button',
+            { onClick: deleteOne },
+            'Decrement'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset'
+        )
+    );
+    ReactDOM.render(templateTwo, appRoute); //1st -> what to render, 2nd param-> where to render
+};
+
+reRender();
--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -2,38 +2,81 @@
 
 console.log("app.js is running!");
 
-var object = {
+const object = {
     title: 'Dish',
-    subtitle: 'dish is a good girl'
+    // subtitle: 'dish is a good girl',
+    options: ['one', 'two']
 }
 
 //JSX
-var template = (        //this paranthesis is not actually required. just for clarity purposes.
+const template = (        //this paranthesis is not actually required. just for clarity purposes.
     <div>
         <h1>{object.title}</h1>
-        <p>{object.subtitle}</p>
-       {/* <ol>
+        {object.subtitle && <p>{object.subtitle}</p>}
+        <p>{object.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
             <li>List item 1</li>
             <li>list item 2</li>
-       </ol> */}
+        </ol>
     </div>
 );
 
-var user = {
-    name: 'Disha',
-    age: 21,
-    location: 'vaishali'
+
+// const user = {
+//     name: 'Disha',
+//     age: 21,
+//     // location: 'vaishali'
+// }
+
+// function getLocation(location){
+//     if(location){
+//         return <p>Location: {location}</p>
+//     }
+//     //if location does not exist, then don't print anthing regarding location.
+// }
+
+// const templateTwo = (
+  
+//     // jsx expressions must have a parent element. if we dont add this <div> as a parent element, there'll be an err
+//     <div>           
+//         <h1>{user.name}</h1>
+//         <p>Age: {user.age}</p>
+//         {getLocation(user.location)}
+
+//         {/*<h1>this is my h1 inside a pair of {`{}`} in jsx expression</h1>*/}
+//     </div>
+// )
+
+
+//to understand events and attributes
+let count = 0;
+const addOne = ()=> {
+    count++;
+    reRender();
+};
+const deleteOne =() => {
+    count--;
+    reRender();
+}
+const reset = () => {
+    count = 0;
+    reRender();
 }
 
-var templateTwo = (
-  
-    // jsx expressions must have a parent element. if we dont add this <div> as a parent element, there'll be an err
-    <div>           
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
-    </div>
-)
+const appRoute = document.getElementById('appDiv');
 
-var appRoute = document.getElementById('appDiv');
-ReactDOM.render(template, appRoute);      //1st -> what to render, 2nd param-> where to render
+//after changing the count val, updated count is not rendered. So, we have to render templateTwo initially and after updating the count.
+const reRender = () => {
+    const templateTwo = (
+        <div>
+            <h1>count: {count}</h1>
+            <button onClick = {addOne}>Increment</button>
+            <button onClick = {deleteOne}>Decrement</button>
+            <button onClick = {reset}>Reset</button>
+        </div>
+    );
+    ReactDOM.render(templateTwo, appRoute);      //1st -> what to render, 2nd param-> where to render
+
+}
+
+reRender();
