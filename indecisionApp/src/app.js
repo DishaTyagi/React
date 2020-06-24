aeec5c0451889a0d4f8a6d7be3190bc7dda271c0
DishaTyagi <omgdisha@gmail.com>
2020-06-24 22:51:23 +0530

    removed webpack cli and installed sass and node loader

--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {IndecisionApp} from './components/IndecisionApp.js';
-
-// const Layout = (props) => {
-//     return (
-//         <div>
-//             <p>hi i am header</p>
-//             {props.children}        
-//             <p>hi i am footer</p>
-//         </div>
-//     )
-// }
-
-// ReactDOM.render((
-//     <Layout>
-//         <div>   
-//             <h2>title</h2>
-//             <h3>i am the content</h3>
-//         </div>
-//     </Layout>
-//     ), document.getElementById('appDiv')
-// );
+import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('appDiv'));
