c33ba41df479f9aa4c4fbf48720ec1dc2eb630e6
DishaTyagi <omgdisha@gmail.com>
2020-06-25 21:15:59 +0530

    react modals styling and mobile considerations

--- indecisionApp/src/components/OptionModal.js
+++ indecisionApp/src/components/OptionModal.js
@@ -7,11 +7,18 @@ const OptionModal = (props) => {
             isOpen={!!props.selectedOption}       /*isOpen is boolean and tells whether the modal should be open or not. */
             onRequestClose={props.handleClearSelectedOption}    //when esc or background is clicked, the modal goes away just like in gmail or drive
             contentLabel='Selected Option'         /*used for accessibility purposes, not gonna show up in browser */
+            closeTimeoutMS = {200}  
+            className = "modal"  
         >
-            <h3>Selected Option</h3>
+            <h3 className="modal__title">Selected Option</h3>
 
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.handleClearSelectedOption}>Okay</button>
+            {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+            <button 
+                className="button" 
+                onClick={props.handleClearSelectedOption}
+            >
+                Okay
+            </button>
         </Modal>
     )
 }
